26b1e6f8c97791560df8de847f2ae451b5fac6bd
Anderson <48361666+andilegal@users.noreply.github.com>
2019-09-05 15:50:49 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<meta charset="utf-8"/>
+<title>Curso PHP</title>
+</head>
+<body>
+<?php
+	echo 'Utilizando a tag padrão';
+?>
+<?= dsbljbllsdn ?>
+<br>
+<?= 'Utkflilizando php' ?>
+<br>
+<? echo 'Utilizando a tag curta' ?>
+</body>
+</html>
---
+++ variaveis.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8"/>
+	<title>Curso PHP</title>
+</head>
+<body>
+<?php
+//String
+$nome = 'Jorge Sant Ana'
+<?= 'utilizando' ?>
+?>
+<h1>Ficha cadastral </h1>
+<br/>
+<p>Nome: <?php print $nome ?></p>
+
+</body>
+</html>
